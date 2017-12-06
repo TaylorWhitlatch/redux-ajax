3189c6c3fa2eba286579262e29ac5bb23b050463
whit121 <Taylorwhitlatch112@gmail.com>
2017-12-06 11:21:59 -0500

    initial

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// Redux stuff
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers/index';
+
+const theStore = createStore(reducers)
+
+
+ReactDOM.render(
+	<Provider store={theStore}>
+		<App />
+	</Provider>
+	, document.getElementById('root'));
+
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import WeatherReducer from './WeatherReducer'
+
+
+const rootReducer = combineReducers({
+	weather: WeatherReducer,
+
+
+})
+
+export default rootReducer;
+
